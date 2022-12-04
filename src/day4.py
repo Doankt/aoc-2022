c81bebe0316f70cecee9e2519f1021f960ea1388
Doankt <36061217+Doankt@users.noreply.github.com>
2022-12-03 21:47:14 -0800

    Add day 4 solutions

--- src/day4.py
+++ src/day4.py
@@ -1,10 +1,26 @@
 from aocparser import *
 
 def solution1():
-    pass
+    total = 0
+    for line in read_input(4):
+        left, right = [l.split("-") for l in line.strip("\n").split(",")]
+        left = {i for i in range(int(left[0]), int(left[1]) + 1)}
+        right = {i for i in range(int(right[0]), int(right[1]) + 1)}
+
+        if left.issubset(right) or right.issubset(left):
+            total += 1
+    return total
 
 def solution2():
-    pass
+    total = 0
+    for line in read_input(4):
+        left, right = [l.split("-") for l in line.strip("\n").split(",")]
+        left = {i for i in range(int(left[0]), int(left[1]) + 1)}
+        right = {i for i in range(int(right[0]), int(right[1]) + 1)}
+
+        if len(left.intersection(right)) >= 1:
+            total += 1
+    return total
 
 if __name__ == "__main__":
     print("Solution 1:", solution1())

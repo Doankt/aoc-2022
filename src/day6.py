8fae8fffc7743ec58cafbe8d623e98c09765125a
Doankt <36061217+Doankt@users.noreply.github.com>
2022-12-07 23:25:26 -0800

    Add day 6 solutions

--- src/day6.py
+++ src/day6.py
@@ -1,10 +1,22 @@
 from aocparser import *
 
+def find_window(window_size: int):
+    with open("inputs/day6.txt", "r") as file:
+        data = file.read()
+    shift = 0
+    while shift <= len(data) - window_size:
+        window = data[shift:shift+window_size]
+
+        if len(set(window)) == window_size:
+            print(window)
+            return shift + window_size
+        shift += 1
+
 def solution1():
-    pass
+    return find_window(4)
 
 def solution2():
-    pass
+    return find_window(14)
 
 if __name__ == "__main__":
     print("Solution 1:", solution1())

5aa90a6490cbbefda9ce151a472582ab8b390cf9
Doankt <36061217+Doankt@users.noreply.github.com>
2022-12-10 22:19:20 -0800

    Change naming to use 2 digits

--- src/aocparser.py
+++ src/aocparser.py
@@ -1,26 +1,25 @@
-def read_input(day_num: int, test=False):
-    if test:    fstring = f"inputs/day{day_num}/day{day_num}_test.txt"
-    else:       fstring = f"inputs/day{day_num}/day{day_num}.txt"
+def _get_fstring(day_num, test=False):
+    if test:    return f"inputs/day{day_num:02d}/day{day_num:02d}_test.txt"
+    else:       return f"inputs/day{day_num:02d}/day{day_num:02d}.txt"
 
+def read_input(day_num: int, test=False):
+    fstring = _get_fstring(day_num, test)
     with open(fstring, "r") as f:
         for line in f:
             yield line
 
 def get_input(day_num: int, test=False):
-    if test:    fstring = f"inputs/day{day_num}/day{day_num}_test.txt"
-    else:       fstring = f"inputs/day{day_num}/day{day_num}.txt"
-        
+    fstring = _get_fstring(day_num, test)
     with open(fstring, "r") as f:
         return f.readlines()
 
 def aoc_open(day_num, test=False):
     try:
-        if test:    return open(f"inputs/day{day_num}/day{day_num}_test.txt", "r")
-        else:       return open(f"inputs/day{day_num}/day{day_num}.txt", "r")
+        return open(_get_fstring(day_num, test), "r")
     except FileNotFoundError as e:
         if test:
-            print(f"Test file for day {day_num} not found.")
+            print(f"Test file for day {day_num:02d} not found.")
         else:
-            print(f"Input file for day {day_num} not found.")
-    finally:
+            print(f"Input file for day {day_num:02d} not found.")
+    except Exception as e:
         exit(1)
